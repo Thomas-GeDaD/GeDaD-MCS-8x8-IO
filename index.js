d037f94d0454c355a1e60d089bcb749e6808027d
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2021-11-29 10:47:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 const packageJson = require('./package')
 const id = packageJson.name.replace(/[-@/]/g, '_')
 const {name, description} = packageJson
+import getdevices from "IO_search.js"
+var MCP23017 = require('node-mcp23017');
+
+//get config from server part
+
+
+//get all availibe devices
+var availabledevices = getdevices()
+
+
+//combine config with availible devices
+
+// throw an errer if a device is configurated but not reachable
+
+//config all devices with default settings, for each availible device:
+for (var adress in availabledevices) { 
+  if (availabledevices[adress].state)
+    eval( " var mcp = new MCP23017\
+            ({address: 0x20, //default: 0x20\
+            device: '/dev/i2c-1',\
+            debug: false\
+            });"   
+)
+    for (var i = 0; i < 16; i++) {
+    (i<=7) ? mcp.pinMode(i, mcp.INPUT) : mcp.pinMode(i, mcp.OUTPUT)
+    }
+  }
+// get config from props, wenn vorher in App Api geschrieben?
+// get config from api before
+
+
+// combine availible devices with config if already done and actual state
+
+
 
 module.exports = function (app) {
   const onStop = []
   let state = 'initial state'
 
   function start(configuration) {
-    state = `yes, I'm started`
-    let interval = setInterval(() => {
-      app.debug('Running ', new Date())
-    }, 5000)
-    onStop.push(() => clearInterval(interval))
+    //use config with state to map to the deltas
   }
 
   function stop() {
-    onStop.forEach((f) => {
-      try {
-        f()
-      } catch (e) {
-        app.error(e)
-      }
-    })
-    state = `now I'm stopped`
+    //stop all running services
   }
 
   const schema = {}
+
+
 
   function registerWithRouter(router) {
     router.get('/doit', (req, res, next) => {
